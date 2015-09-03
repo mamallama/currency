158b90069b03b555e9b6edf429c9695e5f47de03
Llamamama <reneeq25@gmail.com>
2015-09-03 15:58:42 -0400

    Custom error message when currency codes differ

--- currency.rb
+++ currency.rb
@@ -1,3 +1,5 @@
+require './differentcurrencyerror'
+
 class Currency
   attr_accessor :amount, :code
 
@@ -15,14 +17,18 @@ class Currency
   end
 
   def - (currency_1)
-    @code == currency_1.code
-    @amount - currency_1.amount
-    #else bring up Error message
+    if @code == currency_1.code
+      @amount - currency_1.amount
+    else
+      raise DifferentCurrencyCodeError
+    end
   end
 
   def + (currency_1)
-    @code == currency_1.code
-    @amount + currency_1.amount
-    #else bring up Error message
+    if @code == currency_1.code
+      @amount + currency_1.amount
+    else
+      raise DifferentCurrencyCodeError
+    end
   end
 end
