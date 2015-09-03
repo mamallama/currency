63f7d0a55b8dab5c33f8ba2cdccd9af7a6f92296
Llamamama <reneeq25@gmail.com>
2015-09-03 14:14:41 -0400

    currencies with the same amount and currency code are equal

--- currency.rb
+++ currency.rb
@@ -1,10 +1,16 @@
 class Currency
+  attr_accessor :amount, :code
+
   def initialize (amount, code)
     @amount = amount
     @code = code
   end
 
-  def same_a_c
-    
+  def == (currency_1)
+    @amount == currency_1.amount && @code == currency_1.code
+  end
 
+  def add (currency_1)
+    @amount += currency_1.amount.to_i && @code += currency_1.code.to_i
+  end
 end
